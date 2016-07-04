4c1dded9bccd31575809cb9173c8b45a38027dc3
jacopofar <jacopo1.farina@gmail.com>
2016-07-04 18:39:21 +0200

    add hyponyms and the adverb POS

--- src/main/java/com/github/jacopofar/wordnetservice/Server.java
+++ src/main/java/com/github/jacopofar/wordnetservice/Server.java
@@ -11,6 +11,7 @@ import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import net.sf.extjwnl.JWNLException;
 import net.sf.extjwnl.data.*;
+import net.sf.extjwnl.data.list.PointerTargetNode;
 import net.sf.extjwnl.data.list.PointerTargetNodeList;
 import net.sf.extjwnl.dictionary.Dictionary;
 import spark.Response;
@@ -21,6 +22,7 @@ import java.util.stream.Collectors;
 import static spark.Spark.*;
 
 public class Server {
+    enum Relationships {HYPERNYM, HYPONYM};
     static Dictionary dictionary;
     public static void main(String[] args) throws JWNLException {
 
@@ -55,34 +57,34 @@ public class Server {
                 response.status(400);
                 return "invalid request body. Errors: " + ar.errorMessages() ;
             }
+            Map<String, String> params = getParams(ar.getParameter());
+
+            if(!params.containsKey("w")){
+                response.status(400);
+                return "No word parameter found, it should be something like w=cat or pos=n,w=cat" ;
+            }
+
+
             List<POS> accepted_pos = null;
             List<Word> matchWords = new LinkedList<>();
-            //the user can provide a word with no POS (all the matches will be used) or a POS and the word
-            String[] params = ar.getParameter().split(" ");
-            if(params.length == 2){
-                String wordType = params[0];
-                if(!wordType.matches("(adjective)|(noun)|(verb)|a|n|v")){
+            //the user can provide the POS or not
+            if(params.containsKey("pos")){
+                String wordType = params.get("pos").toLowerCase();
+                if(!wordType.matches("(adjective)|(noun)|(verb)|(adverb)|(adv)|n|v|(adj)")){
                     response.status(400);
-                    return "invalid request, the word type has to be adjective, noun or verb, or a, n, v. This was " + wordType ;
+                    return "invalid request, the word type has to be adjective, adverb, noun or verb, or adj, adv, n, v. This was " + wordType ;
                 }
                 accepted_pos = new LinkedList<>();
-                if(wordType.startsWith("a"))
+                if(wordType.startsWith("adv"))
+                    accepted_pos.add(POS.ADVERB);
+                if(wordType.startsWith("adj"))
                     accepted_pos.add(POS.ADJECTIVE);
                 if(wordType.startsWith("v"))
                     accepted_pos.add(POS.VERB);
                 if(wordType.startsWith("n"))
                     accepted_pos.add(POS.NOUN);
-
-                matchWords = getRelated(params[1], "hypernym", accepted_pos, Integer.parseInt(request.params(":senses")));
             }
-            if(params.length == 1){
-                matchWords = getRelated(params[0], "hypernym", null, Integer.parseInt(request.params(":senses")));
-            }
-
-            if(params.length != 1 && params.length != 2){
-                response.status(400);
-                return "invalid parameter. Must be in the form 'POS word' (e.g. 'name cat') or just a word (e.g. 'cat')" ;
-            }
+            matchWords = getRelated(params.get("w"), Relationships.HYPERNYM, accepted_pos, Integer.parseInt(request.params(":senses")));
 
             List<Annotation> anns = new ArrayList<>();
 
@@ -110,7 +112,7 @@ public class Server {
             ArrayNode annotationArray =  JsonNodeFactory.instance.arrayNode();
 
 
-            getRelated(request.params(":word"), "hypernym", null, Integer.parseInt(request.params(":senses"))).stream().forEach(sw -> {
+            getRelated(request.params(":word"), Relationships.HYPERNYM, null, Integer.parseInt(request.params(":senses"))).stream().forEach(sw -> {
                 ObjectNode hyp = JsonNodeFactory.instance.objectNode();
                 hyp.put("POS", sw.getPOS().getLabel());
                 hyp.put("word", sw.getLemma());
@@ -120,6 +122,78 @@ public class Server {
             response.type("application/json; charset=utf-8");
             return annotationArray.toString();
         });
+
+        /**
+         * List the hyponyms of a given word
+         * */
+        get("/hyponyms/:senses/:word", (request, response) -> {
+
+            ArrayNode annotationArray =  JsonNodeFactory.instance.arrayNode();
+            getRelated(request.params(":word"), Relationships.HYPONYM, null, Integer.parseInt(request.params(":senses"))).stream().forEach(sw -> {
+                ObjectNode hyp = JsonNodeFactory.instance.objectNode();
+                hyp.put("POS", sw.getPOS().getLabel());
+                hyp.put("word", sw.getLemma());
+                annotationArray.add(hyp);
+            });
+
+            response.type("application/json; charset=utf-8");
+            return annotationArray.toString();
+        });
+
+
+        post("/hyponyms_tagger/:senses", (request, response) -> {
+            ObjectMapper mapper = new ObjectMapper();
+            AnnotationRequest ar = mapper.readValue(request.body(), AnnotationRequest.class);
+            if(ar.errorMessages().size() != 0){
+                response.status(400);
+                return "invalid request body. Errors: " + ar.errorMessages() ;
+            }
+            Map<String, String> params = getParams(ar.getParameter());
+
+            if(!params.containsKey("w")){
+                response.status(400);
+                return "No word parameter found, it should be something like w=cat or pos=n,w=cat" ;
+            }
+
+
+            List<POS> accepted_pos = null;
+            List<Word> matchWords = new LinkedList<>();
+            //the user can provide the POS or not
+            if(params.containsKey("pos")){
+                String wordType = params.get("pos").toLowerCase();
+                if(!wordType.matches("(adjective)|(noun)|(verb)|(adverb)|(adv)|n|v|(adj)")){
+                    response.status(400);
+                    return "invalid request, the word type has to be adjective, adverb, noun or verb, or adj, adv, n, v. This was " + wordType ;
+                }
+                accepted_pos = new LinkedList<>();
+                if(wordType.startsWith("adv"))
+                    accepted_pos.add(POS.ADVERB);
+                if(wordType.startsWith("adj"))
+                    accepted_pos.add(POS.ADJECTIVE);
+                if(wordType.startsWith("v"))
+                    accepted_pos.add(POS.VERB);
+                if(wordType.startsWith("n"))
+                    accepted_pos.add(POS.NOUN);
+            }
+            matchWords = getRelated(params.get("w"), Relationships.HYPONYM, accepted_pos, Integer.parseInt(request.params(":senses")));
+
+            List<Annotation> anns = new ArrayList<>();
+
+            String text = ar.getText().toLowerCase();
+            Set<String> distinctWords = matchWords.stream().map(w -> w.getLemma()).collect(Collectors.toSet());
+            distinctWords.forEach(l -> {
+                int lastIndex=0;
+                while(true){
+                    int ind=text.indexOf(l, lastIndex);
+                    if(ind ==- 1)
+                        break;
+                    anns.add(new Annotation(ind, ind + l.length()));
+                    lastIndex=ind+l.length();
+                }
+            });
+            return sendAnnotations(anns, response);
+        });
+
     }
 
     /**
@@ -129,7 +203,7 @@ public class Server {
      * @param accepted_pos the list of POS to be considered, null to use them all
      * @param maxSenseIndex how deep to go in word possible senses. 0 to get only one sense
      * */
-    private static List<Word> getRelated(String origin, String relationship, List<POS> accepted_pos, int maxSenseIndex) throws JWNLException {
+    private static List<Word> getRelated(String origin, Relationships relationship, List<POS> accepted_pos, int maxSenseIndex) throws JWNLException {
         ArrayList<Word> retVal = new ArrayList<>();
         for(POS p: accepted_pos == null ? POS.getAllPOS() : accepted_pos){
             IndexWord w = null;
@@ -146,20 +220,24 @@ public class Server {
                     continue;
                 leftSenses--;
                 PointerTargetNodeList related = null;
-                if(relationship.equals("hypernym")){
+                if(relationship == Relationships.HYPERNYM){
                     related = PointerUtils.getDirectHypernyms(sense);
                     if(related.size() == 0)
                         continue;
                 }
-                if(relationship.equals("hyponym")){
+                if(relationship == Relationships.HYPONYM){
                     related = PointerUtils.getDirectHyponyms(sense);
                 }
                 if(related == null){
                     throw new RuntimeException("unknown lexical relationship " + relationship);
                 }
-                related.get(0).getSynset().getWords().stream().forEach(sw -> {
-                    retVal.add(sw);
-                });
+                for (PointerTargetNode relSynset:related){
+                    relSynset.getSynset().getWords().stream().forEach(sw -> {
+                        retVal.add(sw);
+                    });
+                }
+
+
             }
         }
         return retVal;
@@ -180,5 +258,17 @@ public class Server {
         }
         res.type("application/json; charset=utf-8");
         return retVal.toString();
+    }
+
+    private static Map<String, String> getParams(String rawParameter) {
+        String[] params = rawParameter.split(",");
+        Map<String, String> map = new HashMap<>();
+        for (String param : params) {
+            String[] spl = param.split("=");
+            String name = spl[0];
+            String value = spl[1];
+            map.put(name, value);
+        }
+        return map;
     }
 }
